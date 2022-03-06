587d180fb7f86e97b7ca4e6f9c49c94f1c79635d
Josiel Lopez <jdlopez114@gmail.com>
2022-03-05 20:40:52 -0500

    commit tree 59254321e306b7e7ea11a74697f38230a9b61abe

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*"){
+
+    const part1 = 'You are';
+
+    return function (adj = "special"){
+
+    const part2 = 'worker';
+
+        return `${part1} ${flair}${adj}${flair}!`;
+    }
+
+}
+wrapAdjective()();
